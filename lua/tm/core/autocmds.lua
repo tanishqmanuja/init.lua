9ee8e9983a5e5fd6cc42a07080932c967778a8c4
tanishqmanuja <tanishqmanuja@gmail.com>
2024-01-29 19:57:39 +0530

    feat: update autocmnds

--- lua/tm/core/autocmds.lua
+++ lua/tm/core/autocmds.lua
@@ -1,12 +1,12 @@
-local agroup = vim.api.nvim_create_augroup
-local acmd = vim.api.nvim_create_autocmd
+local auto_group = vim.api.nvim_create_augroup
+local auto_command = vim.api.nvim_create_autocmd
 
 -- Just an alias for quitting
 vim.api.nvim_create_user_command("Q", "q", {})
 
 -- Highlight yanked text
-local highlight_group = agroup("YankHighlight", { clear = true })
-acmd("TextYankPost", {
+local highlight_group = auto_group("YankHighlight", { clear = true })
+auto_command("TextYankPost", {
   callback = function()
     vim.highlight.on_yank()
   end,
