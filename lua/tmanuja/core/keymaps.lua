e49a93f15ce0143b8553e42a9b3fc2b72f543e91
tanishqmanuja <tanishqmanuja@gmail.com>
2024-03-22 14:44:03 +0530

    fix: keymaps

--- lua/tmanuja/core/keymaps.lua
+++ lua/tmanuja/core/keymaps.lua
@@ -55,7 +55,7 @@ vim.keymap.set('v', '<C-s>', '<Esc>:update<CR>', { silent = true })
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>x', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Keybinds to make split navigation easier.
 --  See `:help wincmd` for a list of all window commands
