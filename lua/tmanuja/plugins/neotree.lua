6b99a585cbd0779ac7b04011d2f1b86cfd395100
tanishqmanuja <tanishqmanuja@gmail.com>
2024-03-22 14:26:56 +0530

    feat: changes to neotree

--- lua/tmanuja/plugins/neotree.lua
+++ lua/tmanuja/plugins/neotree.lua
@@ -32,6 +32,7 @@ return {
           visible = true,
           show_hidden_count = true,
           hide_dotfiles = false,
+          hide_gitignored = false,
         },
       },
       window = {
