27652e845696d57b1c3ee3740ce0ef39e4d78969
tanishqmanuja <tanishqmanuja@gmail.com>
2024-05-18 16:15:16 +0530

    feat: add ctrl+y mapping in cmp

--- lua/tmanuja/plugins/cmp.lua
+++ lua/tmanuja/plugins/cmp.lua
@@ -63,6 +63,7 @@ return {
       mapping = {
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<C-y>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<Tab>'] = cmp.mapping(function(fallback)
